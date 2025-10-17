cc92e5d4517678ce898a7f099e298488862000d6
nikita chauhan <nikitachauhan3802@gmail.com>
2025-10-17 03:42:06 -0700

    fix MongoDB connection setup

--- backend/connect.js
+++ backend/connect.js
@@ -4,8 +4,6 @@ const mongoURI='mongodb://127.0.0.1:27017/contactform';
 const ConnectToMongo=async()=>{
     try{
        await mongoose.connect(mongoURI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
     });
     console.log("connected to mongodb")
     }catch(err){
